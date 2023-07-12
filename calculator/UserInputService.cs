836e67d40ba08d09a7f178fdc1d6367d13041b07
Nobeen Islam <nobeen.islam@xdesign.com>
2023-07-12 14:56:15 +0100

    Create function to validate input

---
+++ calculator/UserInputService.cs
@@ -0,0 +1,53 @@
+namespace calculator;
+
+public class UserInputService
+{
+    public UserInput TakeUserInput()
+    {
+        UserInput userInput = new UserInput();
+
+        while (true)
+        {
+            Console.WriteLine("What operation would you like to perform. Please type one of add / subtract / divide / multiply");
+            string operationInput = Console.ReadLine();
+
+            if (Enum.TryParse(operationInput, true, out Operations operation))
+            {
+                userInput.operation = operation;
+                break; //Exit loop if input is valid
+            }
+            
+            Console.WriteLine("Yo, I said add / subtract / divide / multiply");
+        }
+        
+        while (true)
+        {
+            Console.WriteLine("Please put in your first input which should be a number");
+            string firstInputAsString = Console.ReadLine();
+
+            if (int.TryParse(firstInputAsString, out int firstInputAsNum))
+            {
+                userInput.firstInput = firstInputAsNum;
+                break; //Exit loop if input is valid
+            }
+            
+            Console.WriteLine("This ain't a number matey");
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Please put in your second number matey");
+            string secondInputAsString = Console.ReadLine();
+
+            if (int.TryParse(secondInputAsString, out int secondInputAsNum))
+            {
+                userInput.secondInput = secondInputAsNum;
+                break; //Exit loop if input is valid
+            }
+
+            Console.WriteLine("This ain't a number matey");
+        }
+
+        return userInput;
+    }
+}
