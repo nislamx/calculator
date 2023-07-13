ded62b3a0cfbe09f85aab8630165e0953003a716
Nobeen Islam <nobeen.islam@xdesign.com>
2023-07-13 12:04:59 +0100

    Loop prompting for calculation

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -4,4 +4,19 @@ IUserInputService userInputService = new UserInputService();
 
 var calculatorLogic = new CalculatorLogic(userInputService);
 
-calculatorLogic.Calculate();
+Console.WriteLine("Welcome to the Calculator Application! \n");
+Console.WriteLine("If you would like to exit the application, press the X key after completing a calculation. \n");
+
+while (true)
+{
+    calculatorLogic.Calculate();
+    Console.WriteLine("\nPress any key to continue with a new calculation or X to exit");
+
+
+    if (Console.ReadKey(false).Key == ConsoleKey.X)
+    {
+        break;
+    }
+}
+    
+
